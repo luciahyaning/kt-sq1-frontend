1ff486ebb14e1b7b8fda3b35c8be33dd42b7a608
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-08-27 10:33:02 +0800

    chore: update BE endpoint

--- src/app/services/apolloClient.tsx
+++ src/app/services/apolloClient.tsx
@@ -14,9 +14,9 @@ const errorLink = onError(({ graphQLErrors, networkError, response, operation })
 });
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/api/graphql',
+  // uri: 'http://localhost:4000/api/graphql',
   // uri: 'http://localhost:4000/api/graphiql',
-  // uri: 'https://another-massive-boilweevil.gigalixirapp.com',
+  uri: 'https://another-massive-boilweevil.gigalixirapp.com/api/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
