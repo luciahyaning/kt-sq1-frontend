b7f576331ab445a2170308d19492bb328e8e1c24
AlfalahM273 <85719232+AlfalahM273@users.noreply.github.com>
2021-08-27 17:24:33 +0800

    feat: login (#3)

--- src/utils/protectedRoute.tsx
+++ src/utils/protectedRoute.tsx
@@ -1,24 +1,9 @@
 import { Redirect, Route } from "react-router";
 import { getUserFromToken, isAuthenticated } from "./AuthHelper";
+import { isTranporter } from "./RoleHelper";
 
 export default function ProtectedRoute({ ...routeProps }) {
   if (isAuthenticated()) {
-    // if (
-    //   getUserFromToken().roleId > 0 &&
-    //   String(routeProps.path).includes("carrot")
-    // ) {
-    //   return <Redirect to={{ pathname: "/" }} />;
-    // } else if (
-    //   !(getUserFromToken().roleId <= 2) &&
-    //   String(routeProps.path).includes("group")
-    // ) {
-    //   return <Redirect to={{ pathname: "/" }} />;
-    // } else if (
-    //   getUserFromToken().roleId > 0 &&
-    //   String(routeProps.path).includes("user")
-    // ) {
-    //   return <Redirect to={{ pathname: "/" }} />;
-    // } else return <Route {...routeProps} />;
     return <Route {...routeProps} />;
   } else {
     return <Redirect to={{ pathname: "login" }} />;
@@ -29,5 +14,14 @@ export function PrivateRoute({ ...routeProps }) {
   //if already logged in can not go to login page
   if (isAuthenticated()) {
     return <Redirect to={{ pathname: "/" }} />;
+    // <Route {...routeProps} />
   } else return <Route {...routeProps} />;
+}
+
+
+export function TranporterRoute({ ...routeProps }) {
+  //if already logged in can not go to login page
+  if (isTranporter()) {
+    return <Route {...routeProps} />;
+  } else return <Redirect to={{ pathname: "login" }} />;
 }
